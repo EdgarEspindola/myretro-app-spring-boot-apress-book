a8eaf4d30d3dbdf1dfa0f9605d3c69a016ed9d4e
Edgar Eduardo Espindola <edgareduardoespindola@gmail.com>
2025-07-04 19:56:50 -0600

    Pasando propiedas con la anotacion: value

--- src/main/java/com/example/myretro/MyRetroConfiguration.java
+++ src/main/java/com/example/myretro/MyRetroConfiguration.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.CommandLineRunner;
@@ -17,34 +18,20 @@ import org.springframework.context.annotation.Configuration;
 public class MyRetroConfiguration {
     Logger log = LoggerFactory.getLogger(MyRetroConfiguration.class);
 
-    MyRetroConfiguration(ApplicationArguments arguments) {
-        log.info("Option Args: {}", arguments.getOptionNames());
-        log.info("Option Arg Values: {}", arguments.getOptionValues("option"));
-        log.info("Non Option: {}", arguments.getNonOptionArgs());
-    }
+    @Value("${users.server}")
+    String server;
+    @Value("${users.port}")
+    Integer port;
+    @Value("${users.username}")
+    String username;
+    @Value("${users.password}")
+    String password;
 
-    @Bean //2
-    CommandLineRunner commandLineRunner() {
-        return args -> {
-
-            log.info("[CLR] Args: {}", Arrays.toString(args));
-        };
-    }
-
-    @Bean //1
-    ApplicationRunner applicationRunner() {
-        return args -> {
-            log.info("[AR] Option Args: {}", args.getOptionNames());
-            log.info("[AR] Option Arg values: {}", args.getOptionValues("option"));
-            log.info("[AR] Non option: {}", args.getNonOptionArgs());
-        };
-    }
-
-    @Bean //3
-    ApplicationListener<ApplicationReadyEvent> applicationReadyEventApplicationListener() {
+    @Bean
+    ApplicationListener<ApplicationReadyEvent> init() {
         return event -> {
-            log.info("[AL] Im ready to interact...");
+            log.info("\nThe users service properties are:\n- Server: {}\n- Port: {}\n- Username: {}\n- Password: {}",
+                    server, port, username, password);
         };
     }
-
 }
